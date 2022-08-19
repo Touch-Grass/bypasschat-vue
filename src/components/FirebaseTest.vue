59a33d4691b6861f4f2800b2c25f1a3f4e51d8c4
Touch-Grass <toberock101@gmail.com>
2022-08-18 19:21:47 -0700

    Added firebase test
    Co-authored-by: Pro Coder <AwesomeMarley@users.noreply.github.com>

---
+++ src/components/FirebaseTest.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div @click="setTest" class="">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { database } from "../main";
+import { ref, set } from "firebase/database";
+export default defineComponent({
+  data() {
+    return {
+      //       set(ref(database, "Testing"), {
+      //           test: "Lets Go!",
+      //   });
+    };
+  },
+  components: {},
+  methods: {
+    setTest() {
+      alert("Updated!");
+      set(ref(database, "Testing"), {
+        test: "The most modern test!",
+      });
+    },
+  },
+});
+</script>
+
+<style scoped>
+button {
+  background-color: blue;
+  width: max-content;
+  height: 3rem;
+  border: red;
+}
+</style>
